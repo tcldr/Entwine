e8a2dc26334fd618b077eec4f446c854c6123cc8
Tristan Celder <tristan.celder@gmail.com>
2019-06-23 19:53:11 +0200

    auto-update docs

---
+++ .build_docs.sh
@@ -0,0 +1,4 @@
+git submodule update --remote
+
+jazzy --config .entwine.yaml
+jazzy --config .entwine-test.yaml
---
+++ .publish_docs.sh
@@ -0,0 +1,5 @@
+./build_docs.sh
+git add .
+git commit -am "auto-update docs"
+git push
+git status
