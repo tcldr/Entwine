ccf4d6961f988bb88e6aea735ecdd1420b6c69ba
Tristan Celder <tristan.celder@gmail.com>
2019-06-24 17:16:15 +0200

    auto-update docs

--- .build_docs.sh
+++ .build_docs.sh
@@ -1,10 +1,15 @@
-git submodule update --remote
-cd Entwine
-git checkout develop
-cd ..
+
+git clone https://github.com/tcldr/Entwine
+git clone https://github.com/tcldr/EntwineRx
+git -C ./Entwine checkout develop
+git -C ./EntwineRx checkout develop
 
 rm README.md
 cp Entwine/README.md README.md
 
 jazzy --config .entwine.yaml
 jazzy --config .entwine-test.yaml
+jazzy --config .entwine-rx.yaml
+
+rm -rf ./Entwine
+rm -rf ./EntwineRx
