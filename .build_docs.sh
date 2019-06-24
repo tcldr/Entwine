cc7dec38ea2c616fd2724396cb7eee2a4ca2acc8
Tristan Celder <tristan.celder@gmail.com>
2019-06-24 18:58:51 +0200

    auto-update docs

--- .build_docs.sh
+++ .build_docs.sh
@@ -1,8 +1,6 @@
 
 git clone https://github.com/tcldr/Entwine
 git clone https://github.com/tcldr/EntwineRx
-git -C ./Entwine checkout develop
-git -C ./EntwineRx checkout develop
 
 rm README.md
 cp Entwine/README.md README.md
